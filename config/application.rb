c30dba5049bdd35d9f4cb769d2650461d3e444bc
Arina Zaitseva <58284411+Avanera@users.noreply.github.com>
2025-06-19 10:45:49 -0300

    configure Rack::Cors

--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,16 @@ module NotesApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # CORS
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins "*"
+        resource "*", headers: :any, methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
+      end
+    end
+
+    # # Rate limiting
+    # config.middleware.use Rack::Attack
   end
 end
